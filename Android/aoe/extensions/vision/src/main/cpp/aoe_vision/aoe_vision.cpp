1c1c881a06c41ed7ce204b09906b4b8350f9aed9
kuloud <xkuloud@gmail.com>
2019-08-27 20:12:47 +0800

    Update aoe_vision module

--- Android/aoe/extensions/vision/src/main/cpp/aoe_vision/aoe_vision.cpp
+++ Android/aoe/extensions/vision/src/main/cpp/aoe_vision/aoe_vision.cpp
@@ -21,48 +21,95 @@ using namespace libyuv;
 extern "C" {
 #endif
 
-
 JNIEXPORT jbyteArray JNICALL
-Java_com_didi_aoe_extensions_vision_AoeVision_NV21Crop(JNIEnv *env, jclass clazz,
-                                                       jbyteArray nv21_src, jint src_width,
-                                                       jint src_height, jint crop_x, jint crop_y,
-                                                       jint crop_width, jint crop_height) {
-    // TODO: implement NV21Crop()
-}
+Java_com_didi_aoe_extensions_vision_AoeVision_NV21ToARGB(JNIEnv *env, jclass clazz,
+                                                         jbyteArray nv21_src,
+                                                         jint src_width, jint src_height,
+                                                         jint degree,
+                                                         jint crop_x, jint crop_y,
+                                                         jint crop_width, jint crop_height,
+                                                         jint out_width, jint out_height) {
+    if (crop_x < 0) {
+        crop_x = 0;
+    }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_didi_aoe_extensions_vision_AoeVision_NV21Rotate(JNIEnv *env, jclass clazz,
-                                                         jbyteArray nv21_src, jint src_width,
-                                                         jint src_height, jint degree) {
-    // TODO: implement NV21Rotate()
-}
+    if (crop_y < 0) {
+        crop_y = 0;
+    }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_didi_aoe_extensions_vision_AoeVision_NV21Resize(JNIEnv *env, jclass clazz,
-                                                         jbyteArray nv21_src, jint src_width,
-                                                         jint src_height, jint out_width,
-                                                         jint out_height) {
-    // TODO: implement NV21Resize()
-}
+    crop_height = crop_height >> 1 << 1;
+    out_height = out_height >> 1 << 1;
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_didi_aoe_extensions_vision_AoeVision_NV21Convert(JNIEnv *env, jclass clazz,
-                                                          jbyteArray nv21_src, jint src_width,
-                                                          jint src_height, jint degree, jint crop_x,
-                                                          jint crop_y, jint out_width,
-                                                          jint out_height) {
-    // TODO: implement NV21Convert()
-}
+    int cropWidth = crop_width;
+    int cropHeight = crop_height;
+    if (crop_x + cropWidth > src_width) {
+        cropWidth = src_width - crop_x;
+    }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_didi_aoe_extensions_vision_AoeVision_NV21ToRGBA(JNIEnv *env, jclass clazz,
-                                                         jbyteArray nv21_src, jint src_width,
-                                                         jint src_height, jint degree, jint crop_x,
-                                                         jint crop_y, jint out_width,
-                                                         jint out_height) {
-    // TODO: implement NV21ToRGBA()
-}
+    if (crop_y + cropHeight > src_height) {
+        cropHeight = src_height - crop_y;
+    }
 
+    jbyte *nv21SrcData = env->GetByteArrayElements(nv21_src, NULL);
+    int responseDimens = (cropWidth * cropHeight * 3) >> 1;
+
+    int dstWidth = cropWidth;
+    int dstHeight = cropHeight;
+    if (degree == 90 || degree == 270) {
+        dstWidth = cropHeight;
+        dstHeight = cropWidth;
+    }
+
+    // 角度
+    const enum RotationMode rotateMode = (enum RotationMode) degree;
+
+    // 裁剪和旋转
+    uint8 *clipResult = new uint8[responseDimens];
+    libyuv::ConvertToI420((const uint8 *) nv21SrcData, (src_width * src_height * 3) >> 1,
+                          clipResult,
+                          dstWidth,
+                          clipResult + dstWidth * dstHeight, dstWidth >> 1,
+                          clipResult + ((dstWidth * dstHeight * 5) >> 2), dstWidth >> 1, crop_x,
+                          crop_y, src_width, src_height,
+                          cropWidth, cropHeight,
+                          rotateMode,
+                          libyuv::FOURCC_NV21);
+
+    responseDimens = out_width * out_height * 4;
+    uint8 *rgba = new uint8[responseDimens];
+
+    if (dstWidth == out_width && dstHeight == out_height) {
+
+        libyuv::I420ToRGBA(clipResult, out_width, clipResult + out_width * out_height, out_width >> 1,
+                           clipResult + ((out_width * out_height * 5) >> 2), out_width >> 1, rgba,
+                           out_width * 4, out_width, out_height);
+    } else {
+        // 需要缩放
+        uint8 *scaleResult = new uint8[out_width * out_height * 3 / 2];
+        libyuv::I420Scale(clipResult, dstWidth,
+                          clipResult + dstWidth * dstHeight, dstWidth / 2,
+                          clipResult + dstWidth * dstHeight * 5 / 4, dstWidth / 2,
+                          dstWidth, dstHeight,
+                          scaleResult, out_width,
+                          scaleResult + out_width * out_height, out_width / 2,
+                          scaleResult + out_width * out_height * 5 / 4, out_width / 2,
+                          out_width, out_height, FilterMode::kFilterBilinear);
+
+        libyuv::I420ToRGBA(scaleResult, out_width, scaleResult + out_width * out_height, out_width >> 1,
+                           scaleResult + ((out_width * out_height * 5) >> 2), out_width >> 1, rgba,
+                           out_width * 4, out_width, out_height);
+
+        delete[] scaleResult;
+    }
+
+    jbyteArray response = env->NewByteArray(responseDimens);
+    env->SetByteArrayRegion(response, 0, responseDimens, (jbyte *) rgba);
+    env->ReleaseByteArrayElements(nv21_src, nv21SrcData, 0);
+
+    delete[] clipResult;
+    delete[] rgba;
+    return response;
+}
 
 #ifdef __cplusplus
 }
--- Android/aoe/extensions/vision/src/main/java/com/didi/aoe/extensions/vision/AoeVision.java
+++ Android/aoe/extensions/vision/src/main/java/com/didi/aoe/extensions/vision/AoeVision.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 The AoE Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.didi.aoe.extensions.vision;
 
 import com.didi.aoe.library.logging.Logger;
@@ -10,71 +26,48 @@ public class AoeVision {
     private static final Logger mLogger = LoggerFactory.getLogger("AoeVision");
 
     /**
-     * NV21 图像区域截取
+     * NV21 格式图像转为 ARGB 格式
      *
      * @param nv21Src
      * @param srcWidth
      * @param srcHeight
+     * @return
+     */
+    public static byte[] NV21ToARGB(byte[] nv21Src, int srcWidth, int srcHeight) {
+        return NV21ToARGB(nv21Src, srcWidth, srcHeight, srcWidth, srcHeight);
+    }
+
+    /**
+     * NV21 格式图像缩放为 ARGB 格式图像
+     *
+     * @param nv21Src
+     * @param srcWidth
+     * @param srcHeight
+     * @param outWidth
+     * @param outHeight
+     * @return
+     */
+    public static byte[] NV21ToARGB(byte[] nv21Src, int srcWidth, int srcHeight, int outWidth, int outHeight) {
+        return NV21ToARGB(nv21Src, srcWidth, srcHeight, 0, 0, 0, srcWidth, srcHeight, outWidth, outHeight);
+    }
+
+
+    /**
+     * NV21 格式图像的裁剪、翻转、缩放
+     *
+     * @param nv21Src
+     * @param srcWidth
+     * @param srcHeight
+     * @param rotation   can be 0, 90, 180 or 270.
      * @param cropX
      * @param cropY
      * @param cropWidth
      * @param cropHeight
-     * @return
-     */
-    public static native byte[] NV21Crop(byte[] nv21Src, int srcWidth, int srcHeight, int cropX, int cropY, int cropWidth, int cropHeight);
-
-    /**
-     * NV21 图像旋转
-     *
-     * @param nv21Src
-     * @param srcWidth
-     * @param srcHeight
-     * @param degree
-     * @return
-     */
-    public static native byte[] NV21Rotate(byte[] nv21Src, int srcWidth, int srcHeight, int degree);
-
-    /**
-     * NV21 图像缩放
-     *
-     * @param nv21Src
-     * @param srcWidth
-     * @param srcHeight
      * @param outWidth
      * @param outHeight
-     * @return
+     * @return 0 for successful; -1 for invalid parameter. Non-zero for failure.
      */
-    public static native byte[] NV21Resize(byte[] nv21Src, int srcWidth, int srcHeight, int outWidth, int outHeight);
-
-    /**
-     * NV21 图像转换，复合 截取、旋转和缩放
-     *
-     * @param nv21Src
-     * @param srcWidth
-     * @param srcHeight
-     * @param degree
-     * @param cropX
-     * @param cropY
-     * @param outWidth
-     * @param outHeight
-     * @return
-     */
-    public static native byte[] NV21Convert(byte[] nv21Src, int srcWidth, int srcHeight, int degree, int cropX, int cropY, int outWidth, int outHeight);
-
-    /**
-     * NV21 格式转换为 RGBA
-     *
-     * @param nv21Src
-     * @param srcWidth
-     * @param srcHeight
-     * @param degree
-     * @param cropX
-     * @param cropY
-     * @param outWidth
-     * @param outHeight
-     * @return
-     */
-    public static native byte[] NV21ToRGBA(byte[] nv21Src, int srcWidth, int srcHeight, int degree, int cropX, int cropY, int outWidth, int outHeight);
+    public static native byte[] NV21ToARGB(byte[] nv21Src, int srcWidth, int srcHeight, int rotation, int cropX, int cropY, int cropWidth, int cropHeight, int outWidth, int outHeight);
 
     static {
         try {
