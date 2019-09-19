0b773b72a0e87bbb458e236e8cfa8a99903236e4
kuloud <xkuloud@gmail.com>
2019-09-19 17:59:33 +0800

    Replace 'junit:junit:4.12' -> 'androidx.test.ext:junit:1.1.1'

--- Android/demo/features/mnist/src/androidTest/java/com/didi/aoe/features/mnist/ExampleInstrumentedTest.java
+++ Android/demo/features/mnist/src/androidTest/java/com/didi/aoe/features/mnist/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package com.didi.aoe.features.mnist;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.didi.aoe.features.mnist.test", appContext.getPackageName());
     }
