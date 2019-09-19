8db7a25ac9c33fb9be5631c60165b778b423c123
kuloud <xkuloud@gmail.com>
2019-09-19 18:18:50 +0800

    Fix 'junit:junit:4.12' error

--- Android/aoe/logging/src/androidTest/java/com/didi/aoe/library/logging/ExampleInstrumentedTest.java
+++ Android/aoe/logging/src/androidTest/java/com/didi/aoe/library/logging/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.didi.aoe.library.logging.test", appContext.getPackageName());
     }
